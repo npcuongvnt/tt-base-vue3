cb4361ce5457dc4a052b11814e822ac6f56e170f
thangtrinh <trinhthang1122@gmail.com>
2024-03-13 17:49:19 +0700

    refactor column filter

--- src/views/pages/Example/ExampleList.vue
+++ src/views/pages/Example/ExampleList.vue
@@ -3,176 +3,60 @@ import { ref } from 'vue';
 import { useFormatUtil } from '@/composables';
 import useBaseList from '@/views/base/baseList';
 
+const { formatDate, formatCurrency } = useFormatUtil();
+
+
+const dt = ref();
 const module = 'example';
 const dataKey = 'example_id';
 const columns = 'example_id,example_name,example_date,example_amount,status,is_bool';
 
-const { filters, pagingParams, loading, listRecords, totalRecords, selectedRecords, onPage, onSort, onFilter, onSearchInputKeyDown, add, view, edit, deleteOne, deleteMulti } = useBaseList({ module, dataKey, columns });
-
-//const { t } = useI18n();
-//const store = useStore();
-//const router = useRouter();
-//const route = useRoute();
-//const confirm = useConfirm();
-//const toast = useToast();
-//const { fe2be } = usePagingParam();
-const { formatDate, formatCurrency } = useFormatUtil();
-
-const dt = ref();
-//const loading = ref(false);
-//const listRecords = ref();
-//const totalRecords = ref(0);
-//const selectedRecords = ref();
-
-//const pagingParams = ref({
-//    first: 0,
-//    rows: 10,
-//    sortField: null,
-//    sortOrder: null,
-//    filters: filters.value
-//});
+const columns = ref({
+        example_id: { isKey: true },
+        example_name: { value: '', matchMode: CONSTANT.PrimeConst.FilterMatchMode.CONTAINS },
+        example_date: { value: null, matchMode: CONSTANT.PrimeConst.FilterMatchMode.DATE_IS },
+        example_amount: { value: null, matchMode: CONSTANT.PrimeConst.FilterMatchMode.EQUALS },
+        status: { value: null, matchMode: CONSTANT.PrimeConst.FilterMatchMode.EQUALS },
+        is_bool: { value: null, matchMode: CONSTANT.PrimeConst.FilterMatchMode.EQUALS }
+    });
 
 const statuses = ref(['unqualified', 'qualified', 'new', 'negotiation', 'renewal', 'proposal']);
 
 const getSeverity = (status) => {
     switch (status) {
-        case 'unqualified':
+        case 1:
             return 'danger';
-        case 'qualified':
+        case 2:
             return 'success';
-        case 'new':
+        case 3:
             return 'info';
-        case 'negotiation':
+        case 4:
             return 'warning';
-        case 'renewal':
+        case 5:
             return null;
     }
 };
 
-//onMounted(() => {
-//    loadPagingData();
-//});
-//
-//const loadPagingData = () => {
-//    loading.value = true;
-//    pagingParams.value = { ...pagingParams.value };
-//
-//    let beParam = fe2be(pagingParams.value);
-//
-//    beParam.columns = columns;
-//
-//    //load danh sách
-//    loadData(beParam);
-//
-//    //load tổng số
-//    loadDataSummary(beParam);
-//};
-//
-//const loadData = (payload) => {
-//    if (payload) {
-//        payload['type'] = ENUM.PagingDataType.DATA;
-//    }
-//    store.dispatch(`${module}/paging`, payload).then((res) => {
-//        if (res) {
-//            listRecords.value = res.pageData;
-//        }
-//        loading.value = false;
-//    });
-//};
-//
-//const loadDataSummary = (payload) => {
-//    if (payload) {
-//        payload['type'] = ENUM.PagingDataType.SUMMARY;
-//    }
-//
-//    store.dispatch(`${module}/paging`, payload).then((res) => {
-//        if (res) {
-//            totalRecords.value = res.total;
-//        }
-//    });
-//};
-//
-//const onPage = (event) => {
-//    pagingParams.value = event;
-//    loadPagingData();
-//};
-//
-//const onSort = (event) => {
-//    pagingParams.value = event;
-//    loadPagingData();
-//};
-//
-//const onFilter = () => {
-//    pagingParams.value.filters = filters.value;
-//    loadPagingData();
-//};
-//
-//const onSearchInputKeyDown = (event) => {
-//    if (event.code === 'Enter' || event.keyCode == 13) {
-//        loadPagingData();
-//    }
-//};
+const { 
+    filters, 
+    pagingParams, 
+    loading, 
+    listRecords, 
+    totalRecords, 
+    selectedRecords, 
+    onPage, onSort, onFilter, onSearchInputKeyDown, 
+    add, 
+    view, 
+    edit, 
+    deleteOne, 
+    deleteMulti 
+} = useBaseList({ module, dataKey, columns });
 
-//const add = () => {
-//    let id = 'id';
-//    router.push({
-//        path: `${route.path}/${id}`,
-//        meta: {
-//            id: id,
-//            data: {}
-//        }
-//    });
-//};//
-
-//const view = (data) => {
-//    debugger
-//};//
-
-//const edit = (data) => {
-//    debugger
-//};//
-
-//const deleteOne = (data) => {
-//    confirmDelete([data]);
-//};//
-
-//const deleteMulti = () => {
-//    let deleteRecords = [...selectedRecords.value];//
-
-//    confirmDelete(deleteRecords);
-//};//
-
-//const confirmDelete = (datas) => {
-//    confirm.require({
-//        header: t('message.HeaderDelete'),
-//        message: t('message.MessageDelete'),
-//        icon: 'pi pi-info-circle',
-//        rejectLabel: t('commandname.CANCEL'),
-//        acceptLabel: t('commandname.DELETE'),
-//        rejectClass: 'p-button-secondary p-button-outlined',
-//        acceptClass: 'p-button-danger',
-//        accept: () => {
-//            let deleteParam = {
-//                Ignores: [],
-//                Models: datas,
-//                Ids: datas.map((i) => i[dataKey])
-//            };//
-
-//            store.dispatch(`${module}/delete`, deleteParam).then((res) => {
-//                if (res) {
-//                    debugger
-//                    toast.add({ severity: 'success', summary: t('message.SummaryAcceptDelete'), detail: t('message.DetailAcceptDelete'), life: 3000 });
-//                }
-//            });
-//        },
-//        reject: () => {
-//            toast.add({ severity: 'info', summary: t('message.SummaryRejectDelete'), detail: t('message.DetailRejectDelete'), life: 2000 });
-//        }
-//    });
-//};
 </script>
 
 <template>
+    <Toast />
+    <ConfirmDialog></ConfirmDialog>
     <div class="grid">
         <div class="col-12">
             <DataTable
@@ -267,8 +151,6 @@ const getSeverity = (status) => {
             </DataTable>
         </div>
     </div>
-    <Toast />
-    <ConfirmDialog></ConfirmDialog>
 </template>
 
 <style scoped lang="scss"></style>
